545a4348346b37fda5369170bfa6897528536f60
Soumith Inturi <sintur2527@gmail.com>
2019-08-15 13:45:10 -0400

    Changed swiper to carousel

---
+++ app/images/teamLogos/logos.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pistons: require('./detroit_pistons.png'),
+  nets: require('./brooklyn-nets.png'),
+  nuggets: require('./denver_nuggets.png'),
+  kings: require('./sacramento_kings.png'),
+};
