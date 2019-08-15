7e4eef93b50bc47288630403d407fb7a2adf972a
Soumith Inturi <sintur2527@gmail.com>
2019-08-15 12:34:49 -0400

    Added team selector screen

--- app/screens/Roster.js
+++ app/screens/Roster.js
@@ -6,24 +6,37 @@ import { API_KEY } from '../config/keys';
 
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-const styles = EStyleSheet.create({});
+const styles = EStyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  name: {
+    fontWeight: '600',
+  },
+  position: {
+    color: '#7A8385',
+  },
+});
 
 const Roster = () => {
   const [roster, setRoster] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const results = await axios({
-        url: `https://api-nba-v1.p.rapidapi.com/players/teamId/10`,
-        method: 'GET',
-        headers: {
-          'x-rapidapi-key': API_KEY,
-        },
-      });
-      setRoster(results.data.api.players);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const results = await axios({
+  //       url: `https://api-nba-v1.p.rapidapi.com/players/teamId/10`,
+  //       method: 'GET',
+  //       headers: {
+  //         'x-rapidapi-key': API_KEY,
+  //       },
+  //     });
+  //     setRoster(results.data.api.players);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <ScrollView>
@@ -34,8 +47,11 @@ const Roster = () => {
             player.leagues.standard.active === '1' ? (
               <ListItem
                 key={i}
+                contentContainerStyle={styles.container}
                 title={`${player.firstName} ${player.lastName}`}
+                titleStyle={styles.name}
                 subtitle={`${player.leagues.standard.pos}`}
+                subtitleStyle={styles.position}
                 bottomDivider={true}
               />
             ) : null
---
+++ app/screens/Selector.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  View,
+  StatusBar,
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import { Image } from 'react-native-elements';
+import Swiper from 'react-native-swiper';
+
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const imageWidth = Dimensions.get('window').width;
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: imageWidth,
+    height: imageWidth / 2,
+  },
+  placeholder: {
+    backgroundColor: '$white',
+  },
+});
+
+const Selector = props => {
+  const handleTeamPress = () => {
+    const { navigation } = props;
+    navigation.navigate('Details');
+  };
+
+  return (
+    <Swiper showsButtons={true} showsPagination={false}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleTeamPress}>
+          <Image
+            style={styles.image}
+            resizeMode="contain"
+            source={require('../images/detroit_pistons.png')}
+            PlaceholderContent={<ActivityIndicator size="large" />}
+            placeholderStyle={styles.placeholder}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleTeamPress}>
+          <Image
+            style={styles.image}
+            resizeMode="contain"
+            source={require('../images/brooklyn-nets.png')}
+            PlaceholderContent={<ActivityIndicator size="large" />}
+            placeholderStyle={styles.placeholder}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleTeamPress}>
+          <Image
+            style={styles.image}
+            resizeMode="contain"
+            source={require('../images/denver_nuggets.png')}
+            PlaceholderContent={<ActivityIndicator size="large" />}
+            placeholderStyle={styles.placeholder}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleTeamPress}>
+          <Image
+            style={styles.image}
+            resizeMode="contain"
+            source={require('../images/sacramento_kings.png')}
+            PlaceholderContent={<ActivityIndicator size="large" />}
+            placeholderStyle={styles.placeholder}
+          />
+        </TouchableOpacity>
+      </View>
+    </Swiper>
+  );
+};
+
+export default Selector;
