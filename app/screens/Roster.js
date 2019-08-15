cb4d3f37936a0e0faace61fa10934b9d21800655
Soumith Inturi <sintur2527@gmail.com>
2019-08-15 10:49:43 -0400

    Created api request for players

--- app/screens/Roster.js
+++ app/screens/Roster.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { View, StatusBar, Text } from 'react-native';
+import { API_KEY } from '../config/keys';
 
 import EStyleSheet from 'react-native-extended-stylesheet';
 
@@ -8,7 +10,21 @@ const styles = EStyleSheet.create({});
 const Roster = () => {
   const [roster, setRoster] = useState({});
 
-  useEffect(() => {});
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios({
+        url: `https://api-nba-v1.p.rapidapi.com/players/teamId/10`,
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': API_KEY,
+        },
+      });
+      setRoster(results.data.api.players);
+    };
+    fetchData();
+  }, []);
+
+  console.log(roster);
 
   return (
     <View>
