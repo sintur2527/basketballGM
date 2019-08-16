35e5201ea13ae61b4e595137042644c121650104
Soumith Inturi <sintur2527@gmail.com>
2019-08-15 23:48:57 -0400

    Simulate single game score

--- app/screens/TeamDetails.js
+++ app/screens/TeamDetails.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StatusBar, Dimensions } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text, Button, Avatar } from 'react-native-elements';
+import moment from 'moment';
 
 import NavBar from '../components/Header';
 import Team from '../components/Team';
@@ -9,13 +10,14 @@ import StatsButton from '../components/StatsButton';
 import ScheduleButton from '../components/ScheduleButton';
 import FinanceButton from '../components/FinanceButton';
 
+import games from '../config/games';
+
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const imageWidth = Dimensions.get('window').width;
 
 const styles = EStyleSheet.create({
   buttons: {
-    flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
     alignItems: 'center',
@@ -32,24 +34,101 @@ const styles = EStyleSheet.create({
     fontWeight: '600',
     paddingBottom: 10,
   },
+  nextGame: {
+    color: '#002147',
+    fontWeight: '600',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  itemWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+  },
+  titleWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#002147',
+  },
+  date: {
+    color: '#7A8385',
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  location: {
+    color: '#7A8385',
+    fontSize: 12,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  button: {
+    width: imageWidth * 0.9,
+    backgroundColor: '#0046ae',
+  },
 });
 
 const TeamDetails = props => {
+  const nextGames = games;
+  // const nextTeam = nextGames[0];
+  // State
+  const [nextTeam, setNextTeam] = useState(nextGames[0]);
+
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+
+  const [awayScore, setAwayScore] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+
+  const [simButton, setSimButton] = useState(false);
+
   const handleRosterPress = () => {
     const { navigation } = props;
     navigation.navigate('Roster');
   };
+
   const handleStatsPress = () => {
     const { navigation } = props;
     navigation.navigate('Stats');
   };
+
   const handleSchedulePress = () => {
     const { navigation } = props;
     navigation.navigate('Schedule');
   };
+
   const handleFinancesPress = () => {
     const { navigation } = props;
     navigation.navigate('Finances');
+  };
+
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  const handleSimulatePress = () => {
+    console.log('simulate game');
+    let away = getRandomInt(90, 121);
+    let home = getRandomInt(90, 121);
+    if (away === home) {
+      getRandomInt(0, 2) === 0 ? away + 10 : home + 10;
+    }
+
+    setAwayScore(away);
+    setHomeScore(home);
+    if (away > home) {
+      setLosses(losses + 1);
+    } else {
+      setWins(wins + 1);
+    }
   };
 
   return (
@@ -59,7 +138,7 @@ const TeamDetails = props => {
       <Team />
       <View style={styles.recordContainer}>
         <Text h2 style={styles.record}>
-          0 - 0
+          {`${wins} - ${losses}`}
         </Text>
       </View>
       <View style={styles.buttons}>
@@ -68,6 +147,26 @@ const TeamDetails = props => {
         <ScheduleButton onPress={handleSchedulePress} />
         <FinanceButton onPress={handleFinancesPress} />
       </View>
+      <Text style={styles.nextGame}>Next Game</Text>
+      <View>
+        <View style={styles.itemWrapper}>
+          <Avatar size="medium" source={{ uri: nextTeam.logo }} />
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>{nextTeam.team}</Text>
+            <Text style={styles.date}>
+              {moment(nextTeam.date).format('dddd, MMM Do')}
+            </Text>
+            <Text style={styles.location}>{nextTeam.city}</Text>
+          </View>
+          <Text style={styles.title}>{`${awayScore} - ${homeScore}`}</Text>
+        </View>
+      </View>
+      <Button
+        containerStyle={styles.buttonContainer}
+        buttonStyle={styles.button}
+        title="Simulate Game"
+        onPress={() => handleSimulatePress()}
+      />
     </View>
   );
 };
